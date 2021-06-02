680790e57624a5a9bdc0dbadb77b8d22b8887afa
wujiahao <jiahao.wu@happy-seed.com>
2021-06-02 21:32:01 +0800

    chore: 添加prettier

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // 缩进字节数
+  tabWidth: 4,
+  // 超过最大值换行
+  printWidth: 100,
+  // 是否使用单引号代替双引号
+  singleQuote: true,
+  // 句末分号
+  semi: false,
+  // 行最大宽
+  printWidth: 80,
+  // 尽可能控制尾随逗号的输出。 有效选项： 'none' - 无尾随逗号 ' es5' - 在ES5中有效的尾随逗号（对象，数组等） 'all' - 尾随逗号 尽可能（函数参数）
+  trailingComma: 'none',
+  // jsx dom 开始标签最后不换行
+  jsxBracketSameLine: true,
+  // (x) => {} 箭头函数参数只有一个时是否要有小括号。avoid：省略括号
+  arrowParens: 'always'
+}
